80aac6f4bafb34441e065a6536cc8741815d5acc
Dave Doolin <david.doolin@gmail.com>
2012-01-31 18:52:40 -0800

    Feature files for behavior of signing in and signing out.

--- features/session.feature
+++ features/session.feature
@@ -1,1 +1,17 @@
-# Session features
+Feature: Users sign in and sign out
+
+  The Sessions features handles page flow corresponding
+  to the Devise model of user authentication. Succinctly,
+  these scenarios describe page flow for signed in users,
+  and what happens when a user signs out.
+
+  Scenario: User signs in
+    Given a user is not signed in
+    Then the user signs in
+    #Then show me the page
+    And is directed to the user's show page
+
+  Scenario: User signs out
+    Given a user is signed in
+    Then the user signs out
+    And is directed to the landing page
--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,1 +1,32 @@
-# Session handling.
+
+Given /^a user is not signed in$/ do
+  visit('/users/sign_out') # ensure that at least
+end
+
+Then /^the user signs in$/ do
+  email = 'testing@man.net'
+  password = 'secretpass'
+  User.new(:email => email, :password => password, :password_confirmation => password).save!
+
+  visit '/users/sign_in'
+  fill_in "user_email", :with=>email
+  fill_in "user_password", :with=>password
+  click_button "Sign in"
+end
+
+Then /^is directed to the user's show page$/ do
+  page.should have_css("div.recipes")
+end
+
+Given /^a user is signed in$/ do
+  step %{the user signs in}
+end
+
+Then /^the user signs out$/ do
+  step %{a user is not signed in}
+end
+
+# TODO: Refactor to combine with frontpage step
+Then /^is directed to the landing page$/ do
+  page.should have_css("body#index")
+end

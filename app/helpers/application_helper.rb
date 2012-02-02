b8b2282bd48b87b62b03a6134cedd5adb1d5bb50
Dave Doolin <david.doolin@gmail.com>
2012-02-02 15:27:40 -0800

    factored out some messy template code into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def hash1
+    {:bread => 'white', :filling => 'peanut butter and jelly'}
+  end
+
+  def hash2
+    {:bread => 'whole wheat'}
+  end
+
+  def template_id
+    {:id => params[:action] }
+  end
+
+  def template_class
+    {:class => "#{params[:controller].gsub(/\//,'-')} #{params[:action]}"}
+  end
 end
